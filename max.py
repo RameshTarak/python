42579a2153ad5a054d8ec42179dbdcad8f1308bc
RameshTarak <90302832+RameshTarak@users.noreply.github.com>
2021-09-16 18:47:11 -1200

    Add files via upload

---
+++ max.py
@@ -0,0 +1,16 @@
+Z=["ramesh","suresh","aravind","venkatesh"]
+len(Z)
+Z.append("rajesh")
+Z.insert(0,"naveen")
+a=["anil","raja","jeans"]
+Z.extend(a)
+Z.insert(5,"vishnu")
+Z.copy()
+del Z[-1]
+Z.copy()
+Z.append(4)
+Z.copy()
+Z.index("raja")
+print(Z[4])
+Z.append("ramesh")
+set(Z)
